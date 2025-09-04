9c2dad626f91ba7189d2c90d14f5f189a71e2856
jane <janethtcenter@gmail.com>
2025-09-04 18:14:50 +0300

    aston

--- src/main/java/HomeWork.java
+++ src/main/java/HomeWork.java
@@ -1,45 +1,90 @@
 public class HomeWork {
-}
+    public static void printThreeWords("Orange","Banana","Apple") {
+        System.out.println("Orange", "Banana", "Apple");
+    }
 
-public static void printThreeWords("Orange", "Banana", "Apple") {
-    System.out.println ("Orange", "Banana", "Apple");
-}
+    public static void printColor("Красный","Желтый","Зеленый") {
+        if (a <= 0) ;
+        {
+            System.out.println("Красный");
+        } else if (b > 0 && b <= 100) {
+            System.out.println("Желтый");
+        } else if (c > 100) {
+            System.out.println("Зеленый");
+        }
+    }
 
-public static void printColor("Красный", "Желтый", "Зеленый") {
-    if (a <= 0),{ //
-        System.out.println("Красный");
-    } else if (b > 0 && b <= 100) { //
-        System.out.println("Желтый");
-    } else if (c > 100) { //
-        System.out.println("Зеленый");
+    public static void compareNumbers(int a, int b) {
+
+        int a = 3;
+        int b = 1;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    public boolean inRange10to20(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+    }
+
+    public static void checkNumber(10,-5,0) {
+        {
+            if (10,-5, 0 >= 0){
+            System.out.println("10 "++" (0).");
+        } else{
+            System.out.println(" "++" -.");
+        }
+        }
+
+        public static boolean main ( int -5){
+            if (-5 < 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public static void printStringNTimes ("Hello", 3);
+        {
+            if (count > 0) {
+                for (int i = 0; i < 3; i++) ;
+                {
+                    System.out.println("Hello");
+                    System.out.println("Hello");
+                    System.out.println("Hello");
+                }
+            }
+        }
+
+        public static boolean isLeapYear (year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+        public static void main (String[]args){
+            int[] arr1 = new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+            for (int i = 0; i < arr1.length; i++) {
+                if (arr1[i] == 0)
+                    arr[i]++;
+                else
+                    arr[i]--;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
 
-public static void compareNumbers(int a, int b) {
-//
-    int a = 3;
-    int b = 1;
-    if (a >= b) {
-        System.out.println("a >= b"); //
-    } else {
-        System.out.println("a < b")
-    }
-}
 
-public boolean inRange10to20(int a, int b) {
-    int sum = a + b;
-    return sum >= 10 && sum <= 20;
-}
 
-public static void checkNumber(int 10, -5, 0) {
-    if (10, -5, 0 >= 0) {
-        System.out.println("10 " ++ " (0).");
-    } else {
-        System.out.println(" " ++ " -.");
-    }
-}
-public static boolean isNegative(10, -5, 0) {
-    return number < 0;
+
+
 
 
 
