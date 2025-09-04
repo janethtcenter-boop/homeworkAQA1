6d42d49ea11f5dcf86e921d0f685655479bedecb
jane <janethtcenter@gmail.com>
2025-09-04 14:29:51 +0300

    aston

---
+++ src/main/java/HomeWork.java
@@ -0,0 +1,48 @@
+public class HomeWork {
+}
+
+public static void printThreeWords("Orange", "Banana", "Apple") {
+    System.out.println ("Orange", "Banana", "Apple");
+}
+
+public static void printColor("Красный", "Желтый", "Зеленый") {
+    if (a <= 0),{ //
+        System.out.println("Красный");
+    } else if (b > 0 && b <= 100) { //
+        System.out.println("Желтый");
+    } else if (c > 100) { //
+        System.out.println("Зеленый");
+    }
+}
+
+public static void compareNumbers(int a, int b) {
+//
+    int a = 3;
+    int b = 1;
+    if (a >= b) {
+        System.out.println("a >= b"); //
+    } else {
+        System.out.println("a < b")
+    }
+}
+
+public boolean inRange10to20(int a, int b) {
+    int sum = a + b;
+    return sum >= 10 && sum <= 20;
+}
+
+public static void checkNumber(int 10, -5, 0) {
+    if (10, -5, 0 >= 0) {
+        System.out.println("10 " ++ " (0).");
+    } else {
+        System.out.println(" " ++ " -.");
+    }
+}
+public static boolean isNegative(10, -5, 0) {
+    return number < 0;
+
+
+
+
+
+
